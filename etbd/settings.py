62f774ae7aff0eeba4e56e57ddbba94ad7b4004d
Joy <jtkaufman737@gmail.com>
2018-03-12 20:18:28 -0400

    Add post model

--- etbd/settings.py
+++ etbd/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#!5+4xraf4dd-!#6(k#xg1h!bn)$-=2%*%g*m6sza9gwapqqm$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','.pythonanywhere.com','.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','localhost','.pythonanywhere.com','.herokuapp.com']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 ]
 
 MIDDLEWARE = [
